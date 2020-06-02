f64b0606171873fa253e78b44919c04666d1f5cf
Dmitriy Kokorev <dima.kokorev@scorum.com>
2020-06-02 14:14:41 +0300

    feat: add stylelint config

---
+++ packages/stylelint-config/scss.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: "stylelint-config-sass-guidelines",
+  rules: {
+    "scss/at-mixin-pattern": null,
+    "max-nesting-depth": null,
+    "scss/at-import-no-partial-leading-underscore": null,
+    "selector-max-compound-selectors": null,
+    "selector-no-qualifying-type": null,
+    "property-no-vendor-prefix": null,
+    "selector-class-pattern": null,
+    "selector-max-empty-lines": 0,
+    "max-empty-lines": 1,
+    "value-list-max-empty-lines": 0,
+  },
+};
